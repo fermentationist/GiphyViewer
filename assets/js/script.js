ff86be2eb7d3bb8bed27626b309749646727374f
fermentationist <fermentationist@gmail.com>
2017-10-04 03:02:13 -0500

    input to create new buttons added. buttons changed to split dropdown, with option to delete.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,84 @@
 $(document).ready(function() {
 	console.log("so far, so good");
 
+	var numberOfGIFs = 25;
+
+	$("#submit-button").on("click", function(){
+		var newButton = $("#new-button").val();
+		newButton = newButton.split(" ").join("_");
+		console.log(newButton);
+		$("#new-button").val("");
+		populateButtons([newButton]);
+
+	});
+
 	var tagArraySample = ["beer", "brewing", "brewer", "brewery", "Duff", "hops", "malt", "barley", "water", "yeast"];
 	
 	function populateButtons(tagArray){
-		var button;
+		var button, removeButton, btnGroup;
 		tagArray.forEach(function(tag){
-			button = $("<button>").attr("id", tag).text(tag);
-			$("#gifArea").append(button);
+			var self = this;
+
+			button = $('<button type="button" class="btn btn-warning">').attr("id", tag).text(tag);
+			btnGroup = $('<div class="btn-group" id="#'+tag+'-group">').append(button);
+			removeButton = '<button type="button" class="btn btn-warning dropdown-toggle" id="'+tag+'-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="caret"></span><span class="sr-only">Toggle Dropdown</span></button><ul class="dropdown-menu" id="'+tag+'-dropdown-menu"><li><a href="#" id="'+ tag +'-delete">delete button</a></li></ul>';
+			btnGroup.append(removeButton);
+			button.on("click", function(event){
+				console.log("tag = " + tag);
+				getGifs(tag, numberOfGIFs);
+			})
+			$(document).on("click", "#"+tag+"-delete", function(){
+				console.log("#"+tag+"-delete clicked");
+				deleteButton(tag);
+			});
+			$("#buttonArea").append(btnGroup);
 	});
 
+	function deleteButton(tag){
+		console.log("deleteButton("+tag+")");
+		$("#"+tag).parent().remove();
+	}
+
 	function getGifs(tag, numberOfGIFs){
+		$("#gifArea").empty();
+		console.log("getGifs(", tag, ", " + numberOfGIFs + ") called");
 		var APIKey = "n0vQqF6LSLU0o20aL1osFF3KBFhB4qhq";
 		var gifURL, stillURL, gifURLsmall, gif, gifStill;
-		var queryURL="http://api.giphy.com/v1/gifs/random?tag=" +tag+ "&api_key=" +APIKey;
+		var queryURL="https://api.giphy.com/v1/gifs/search?q=" + tag + "&limit="+numberOfGIFs+"&api_key=" +APIKey;
 		$.ajax({
-			accept : {
+			accepts : {
 				image: "image/*"
 			},
 			method: "GET",
 			url: queryURL
 		}).done(function(response){
+			var dataArray = response.data;
 			console.log(response);
-			gifURL = response.data.image_url;
-			console.log(gifURL);
-			gifURLsmall = response.data.fixed_height_small_url;
-			console.log(gifURLsmall);
-			stillURL = response.data.fixed_height_small_still_url;
-			console.log(stillURL);
-			// for(datum in response.data){
-			// 	console.log(response.data[datum]);
-			// }
-			gifStill = $("<img>").attr("src", stillURL);
-			console.log("gifStill = ", gifStill);
-			gifStill.on("click", function(event){
-				console.log("click");
-				$(event.target).attr("src", gifURL);
-			})
-			$("#gifArea").append(gifStill);
-	});
+			dataArray.forEach(displayGif);
+			});
 	}
 
+	function displayGif(gif){
+		console.log("gif = "+gif);
+		var stillURL = gif.images.original_still.url;
+		var gifURL = gif.images.original.url;
+		var gifStill = $("<img>").attr("src", stillURL);
+			gifStill.on("click", function(event){
+				console.log("click");
+				if ($(event.target).attr("src") === stillURL){
+					gif = gifURL;
+				}else{
+					gif = stillURL
+				}
+				$(event.target).attr("src", gif);
+			})
+			gifStill.append('<label for="')
+			$("#gifArea").append(gifStill);
+
+	}
+
+
 	};	
-	// populateButtons(tagArraySample,10);
+	populateButtons(tagArraySample);
 });
+
